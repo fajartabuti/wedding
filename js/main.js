9c7c0728460df190c00d16311971bd794d5b9978
Fajar Tri Saputra <fajartrisaputra@gmail.com>
2020-08-27 20:02:45 +0700

    update 1

--- js/main.js
+++ js/main.js
@@ -303,7 +303,7 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("17 October 2020 14:00:00 GMT+07:00");			
+		var endTime = new Date("17 October 2020 15:00:00 GMT+07:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
